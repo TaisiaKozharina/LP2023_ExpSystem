0d4505b3e3af9c268ce7c333635625aaa6038a51
TaisiaKozharina <kozharina1306@gmail.com>
2023-04-07 14:31:33 +0300

    fixes

--- kb2.pl
+++ kb2.pl
@@ -26,7 +26,7 @@ plant('English Ivy', decorative_foilage, '1', '2', yes, high, no, yes).
 plant('Guiana Chestnut', fruiting, '4', '3', no, high, no, no).
 plant('Dragon Tree', decorative_foilage, '4', '3', yes, high, no, yes).
 plant('Monstera', decorative_foilage, '12', '3', yes, high, no, yes).
-plant('Orchid', flowering, '9', '3', no, high, no, yes).
+plant('Orchid', flowering, '9', '3', no, high, no, no).
 plant('Geranium', flowering, '2', '2', no, high, no, yes).
 plant('Peace Lilies', flowering, '4', '3', no, low, no, yes).
 plant('Anthurium', flowering, '13', '2', no, high, no, yes).
@@ -38,82 +38,3 @@ plant('Jasmine', flowering, '9', '2', yes, high, no, yes).
 plant('Begonia', decorative_foliage, '14', '2', yes, low, no, yes).
 plant('Azalea', flowering, '9', '2', yes, low, no, yes).
 plant('Impatien', flowering, '9', '1', yes, low, no, yes).
-
-
-% hasFlowers('Orchid').
-% hasFlowers('Geranium').
-% hasFlowers('Peace Lilies').
-% hasFlowers('Anthurium').
-% hasFlowers('Cyclamen').
-% hasFlowers('Jasmine').
-% hasFlowers('Begonia').
-% hasFlowers('Azalea').
-% hasFlowers('Impatien').
-
-% hasFruit('').
-% hasFruit('Avocado tree').
-% hasFruit('Strawberry').
-
-% isBushy('Cyclamen').
-% isBushy('Jasmine').
-% isBushy('Begonia').
-% isBushy('Azalea').
-% isBushy('Impatien').
-% isBushy('Strawberry').
-
-% leafSize('Bear Paws', 'small').
-% leafSize('Panda Plant', 'small').
-% leafSize('Burro`s Tail', 'tiny').
-% leafSize('Cactus', 'small').
-% leafSize('Burro`s Tail', 'tiny').
-% leafSize('Aloe Vera', 'big').
-
-% leafShape('Asparagus Fern', 'needle-like').
-% leafShape('Boston fern', 'feather-like').
-% leafShape('Bird`s Nest Fern', 'feather-like').
-% leafShape('Calathea', 'round').
-% leafShape('Cast-Iron Plant', 'oblong').
-% leafShape('Chinese Evergreen', 'oval').
-% leafShape('Chinese Money Plant', 'round').
-% leafShape('English Ivy', 'diamond').
-% leafShape('Guiana Chestnut', 'triangle').
-% leafShape('Dragon Tree', 'string').
-% leafShape('Monstera', 'heart').
-% leafShape('Peperomia', 'round').
-% leafShape('Croton', 'oblong').
-
-% hasSpikes('Bear Paws').
-% hasSpikes('Cactus').
-% hasSpikes('Aloe Vera').
-
-% isFluffy('Bear Paws').
-% isFluffy('Panda Plant').
-
-% interestingLeaf('Bird`s Nest Fern').
-% interestingLeaf('Calathea').
-% interestingLeaf('Cast-Iron Plant').
-% interestingLeaf('Chinese Evergreen').
-% interestingLeaf('Peperomia').
-% interestingLeaf('Croton').
-% interestingLeaf('Monstera').
-
-% hasLongStem('Chinese Money Plant').
-% hasLongStem('Peperomia').
-% hasLongStem('Croton').
-% hasLongStem('English Ivy').
-% hasLongStem('Guiana Chestnut').
-% hasLongStem('Dragon Tree').
-% hasLongStem('Monstera').
-% hasLongStem('Dragon Tree').
-% hasLongStem('Orchid').
-% hasLongStem('Geranium').
-% hasLongStem('Peace Lilies').
-% hasLongStem('Anthurium').
-% hasLongStem('Citrus tree').
-% hasLongStem('Avocado tree').
-
-% hasWaterStorage('Bear Paws').
-% hasWaterStorage('Panda Plant').
-% hasWaterStorage('Burro`s Tail').
-% hasWaterStorage('Cactus').
-% hasWaterStorage('Aloe Vera').
